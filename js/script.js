de4567224e269c76cea93c0fda73c7c397babb33
Antonio Donadoni <antonio.donadoni87@gmail.com>
2020-06-24 15:39:12 +0200

    ADD Lista in HTML

--- js/script.js
+++ js/script.js
@@ -10,13 +10,17 @@ var nomeInserito = document.getElementById('cognome').value;
 listaNomi.push(nomeInserito);
 // ORDINO ALFABETICAMENTE
 listaNomi = listaNomi.sort();
-console.log(listaNomi);
+// STAMPO LISTA
+for (var i = 0; i < listaNomi.length; i++) {
+  document.getElementById('lista-completa').innerHTML += "<li>" + listaNomi[i] + "</li>";
+}
+
 //CONTROLLO LA POSIZIONE DEL NOME IN LISTA
 var posizioneLista = 1;
-
 for (var i = 0; i < listaNomi.length && listaNomi[i] != nomeInserito; i++) {
   posizioneLista++;
 }
 // OUTPUT POSIZIONE
+
 document.getElementById('posizione-lista').innerHTML = posizioneLista;
 });
